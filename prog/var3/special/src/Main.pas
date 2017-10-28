e91a8816679ffdce4d27577beb94201479b55ff6
rougsig <rougsig@gmail.com>
2017-10-29 00:10:44 +0200

    - PAINStarted

---
+++ prog/var3/special/src/Main.pas
@@ -0,0 +1,8 @@
+uses UsesPerson;
+
+var
+  p: Person = new Person;
+
+begin
+  writeln('1231231');
+end.
---
+++ prog/var3/special/src/UsesPerson.pas
@@ -0,0 +1,10 @@
+unit UsesPerson;
+interface
+  type Person = class
+  
+  end;
+implementation
+{закрытые описания процедуры и функции }
+begin
+{код инициализации }
+end.
