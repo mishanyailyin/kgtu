30c737b7cc80bc5412a30480d8880b3131095421
rougsig <rougsig@gmail.com>
2017-11-23 23:49:38 +0200

    KR

---
+++ prog/var5/3/3.pas
@@ -0,0 +1,34 @@
+program lab3;
+var
+	x1,
+	y1,
+	x2,
+	y2,
+	result
+		: real;
+
+begin
+	write('x1 -> ');
+	readln(x1);
+	write('y1 -> ');
+	readln(y1);
+
+	write('x2 -> ');
+	readln(x2);
+	write('y2 -> ');
+	readln(y2);
+
+	if(x1 = x2) and (y1 = y2) then begin
+			writeln('--ОШИБКА--');
+			writeln('[x1 : y1] не должен быть равен [x2 : y2]');
+			writeln('---------');
+			exit;
+	end;
+
+	if (x1*y2-x2*y1) = 0 then
+		writeln('проходит через начало координат')
+	else
+		writeln('не проходит через начало координат');
+	result := sqrt(sqr(x2-x1) + sqr(y2-y1));
+	writeln('длина прямой -> ', result:10:3);
+end.
---
+++ prog/var5/4/4.pas
@@ -0,0 +1,55 @@
+program lab4;
+var
+	x1,
+	y1
+		: real;
+
+	x2,
+	y2,
+	y2From,
+	y2To
+		: real;
+
+	step
+		: real;
+
+	result
+		: boolean;
+	_string: string;
+
+begin
+	write('x1 -> ');
+	readln(x1);
+	write('y1 -> ');
+	readln(y1);
+
+	write('x2 -> ');
+	readln(x2);
+	write('y2From -> ');
+	readln(y2From);
+	write('y2To -> ');
+	readln(y2To);
+	if(y2From > y2To) then begin
+			writeln('--ОШИБКА--');
+			writeln('y2From должен быть больше чем y2To');
+			writeln('---------');
+			exit;
+	end;
+
+	step := (y2To - y2From) / 16;
+
+	writeln('| ','----y2----',' | ','--проходит через начало координат?--',' |');
+
+	y2 := y2From;
+	while (y2 <= y2To) do begin
+		result := (x1*y2-x2*y1) = 0;
+		if result then
+			_string := 'проходит через начало координат'
+		else
+			_string := 'не проходит через начало координат';
+		writeln('| ',y2:10:3,' | ',_string:37,' |');
+		y2 += step;
+	end;
+
+	writeln('| ','----------',' | ','-------------------------------------',' |');
+end.
