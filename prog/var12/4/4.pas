c55d3285087a0c59a0044204a7b7c1e871952d9c
mishanyailyin <34071327+mishanyailyin@users.noreply.github.com>
2017-11-28 21:09:40 +0300

    4.1.pas

--- prog/var12/4/4.pas
+++ prog/var12/4/4.pas
@@ -15,21 +15,32 @@ begin
 		writeln('радиус должен быть больше нуля');
 	end else if radiusFrom >= radiusTo then begin
 		writeln('радиусОТ должен быть больше радиусаДО');
-	end else begin
+	end else 
+		begin
 		write('введите координаты точки x y: ');
 		readln(x, y);
-
-		step := (radiusTo - radiusFrom) / 16;
+		
+		writeln('┌──────┬───────────────────────────┬───────────────┐');
+		writeln('│Радиус│Расстояние от точки до окр.│Положение точки│');
+		writeln('├──────┴───────────────────────────┼───────────────┤');
+		
+	        step := (radiusTo - radiusFrom) / 16;
 		radius := radiusFrom;
-		while (radius <= radiusTo) do begin
+		
+		while (radius <= radiusTo) do 
+			begin
+			write('│'. radius:10:3,     '│');
+			
 			result := abs(sqrt(sqr(x)+sqr(y)) - radius);
-			write('расстояние от точки, до окружности равно: ', result:10:3, ' | ');
-			if result > 0 then
-				writeln('точка находится за окружностью')
+			
+			write('result:10:3, ' | ');
+			
+			if result > 0 then 
+				writeln('За окружностью│')
 			else if result = 0 then
-				writeln('точка находится на окружности')
+				writeln('На окружности│')
 			else
-				writeln('точка находится внутри окружности');
+				writeln('Внутри окружности');
 			radius += step;
 		end;
 	end;
